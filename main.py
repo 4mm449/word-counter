f722f5ac43f809aa87627d078f86c2115afe018c
4mm449 <64969293+4mm449@users.noreply.github.com>
2021-03-13 12:58:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #input("THIS IS AN EARLY BUILD OF THIS PROGRAM. FEATURES ARE LIMITED.\nHERE ARE SOME THINGS YOU CAN AND CANNOT DO IN THIS EARLY BUILD: \n1.YOU CANNOT PASTE TEXT FROM THE CLIPBOARD :( \n2.YOU CAN HOWEVER TYPE IN TEXT AND THE PROGRAM WILL WORK AS INTENDED. \nPress Enter (Return) to Continue...")
+input("Press Enter (Return) to Get Started...")
 words = input("ENTER WORDS TO COUNT\n")
 
 calculate_words = words.split()
